10055d463f8801d9e17293be63d24e06e7ba8974
UMRnInside <30196401+UMRnInside@users.noreply.github.com>
2020-10-11 17:14:38 +0800

    Prototype?

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
 const fs = require('fs')
 const mc = require('minecraft-protocol')
 const Injector = require('./mclogger.js').InjectClient
-const config = JSON.parse(fs.readFileSync(process.argv[2] || 'config.json'))
-console.log(1, config)
+//const config = JSON.parse(fs.readFileSync(process.argv[2] || 'config.json'))
 
-function StartClient() {
-    global.client = mc.createClient({
+global.clients = new Array();
+
+function McClient(id, config)
+{
+    this.id = id
+    this.config = config
+}
+McClient.prototype.StartClient = function() {
+    let self = this
+    self.client = mc.createClient({
         host: config.McHost,
         port: config.McPort,
         username: config.McUsername,
         version: config.McVersion
     })
-    Injector(client, config, StartClient)
+    Injector(self.client, config, self.StartClient)
     for (let i in config.AutoRespond)
     {
         const obj = config.AutoRespond[i]
-        client.RegisterPattern(RegExp(obj.pattern), function(p) {
+        self.client.RegisterPattern(RegExp(obj.pattern), function(p) {
             client.chat(obj.response)
         })
     }
 }
 
-StartClient()
+
+const config = JSON.parse(fs.readFileSync(process.argv[2] || 'config.json'))
+var test_client = new McClient(1, config)
+test_client.StartClient()
